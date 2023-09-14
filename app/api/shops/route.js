6e0875a7f5912dd376e6ac42280c2ddd75b16e68
kurniawands <kds.wawan@gmail.com>
2023-09-14 12:51:59 +0700

    api fetch prod and shop

---
+++ app/api/shops/route.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2/promise");
+
+export const GET = async (request) => {
+  try {
+    const conn = await mysql.createConnection({
+      host: "localhost",
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    });
+
+    const [results, fields] = await conn.query("SELECT * FROM `shop` LIMIT 10");
+
+    return new Response(JSON.stringify(results), { status: 200 });
+  } catch (error) {
+    return new Response(error, {
+      status: 500,
+    });
+  }
+};
