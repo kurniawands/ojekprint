069befa5429979929602524c7ddef87b4a51d8ec
kurniawands <kds.wawan@gmail.com>
2023-12-01 01:07:09 +0700

    implement login with next-auth

--- components/LogForm.jsx
+++ components/LogForm.jsx
@@ -1,27 +1,49 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const LogForm = () => {
+  const router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [session, setSession] = useState(true);
+
+  // useEffect(() => {
+  //   console.log(password);
+  // }, [password]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await fetch("/api/users/login", {
-        method: "POST",
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-      });
-    } catch (error) {
-      console.log(error);
+    const response = await signIn("credentials", {
+      username: username,
+      password: password,
+      redirect: false,
+    });
+
+    console.log({ response });
+    if (!response?.error) {
+      router.push("/");
+      router.refresh();
+    } else {
+      setSession(false);
     }
 
-    location.href = "/";
+    // try {
+    //   const response = await fetch("/api/users/login", {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       username: username,
+    //       password: password,
+    //     }),
+    //   });
+    // } catch (error) {
+    //   console.log(error);
+    // }
+
+    // location.href = "/";
   };
 
   return (
@@ -34,12 +56,23 @@ const LogForm = () => {
         <div className="text-xl">Silakan login terlebih dahulu</div>
         <div className="text-xl">Silakan masukkan data di bawah ini</div>
       </div>
-      <div className="flex flex-col w-[480px] gap-4 my-12 text-xl">
+      <div className="flex flex-col w-[480px] gap-4 mt-4 mb-12 text-xl">
+        {!session && (
+          <div className="w-72 self-center bg-red-400 text-center text-white text-sm">
+            Nama Pengguna atau Kata Sandi salah
+          </div>
+        )}
         <div>
-          <div className="px-2">Nama Pengguna</div>
+          {/* <div className="px-2">Nama Pengguna</div> */}
+          <label htmlFor="username" className="px-2">
+            Nama Pengguna
+          </label>
           <div className="h-10 items-center rounded-2xl border border-gray-600">
             <input
               type="text"
+              id="username"
+              name="username"
+              required
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               className="w-full h-full px-3 rounded-2xl"
@@ -47,10 +80,16 @@ const LogForm = () => {
           </div>
         </div>
         <div>
-          <div className="px-2">Kata Sandi</div>
+          {/* <div className="px-2">Kata Sandi</div> */}
+          <label htmlFor="password" className="px-2">
+            Kata Sandi
+          </label>
           <div className="h-10 items-center rounded-2xl border border-gray-600">
             <input
               type="password"
+              id="password"
+              name="password"
+              required
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="w-full h-full px-3 rounded-2xl"
